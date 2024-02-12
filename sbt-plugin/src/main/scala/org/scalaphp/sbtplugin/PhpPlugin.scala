0da92880b6c431dcfdbcd2b349329cfead571587
Jakub Kozłowski <kubukoz@gmail.com>
2024-02-12 02:14:16 +0100

    Add running to sbt plugin

--- sbt-plugin/src/main/scala/org/scalaphp/sbtplugin/PhpPlugin.scala
+++ sbt-plugin/src/main/scala/org/scalaphp/sbtplugin/PhpPlugin.scala
@@ -10,17 +10,32 @@ object PhpPlugin extends AutoPlugin {
 
   object autoImport {
     val scalaPhpVersion = settingKey[String]("The version of scala-php in use")
+    val phpBinary = settingKey[File]("The path to the PHP interpreter binary")
   }
 
   import autoImport._
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     scalaPhpVersion := BuildInfo.version,
+    phpBinary := {
+      import sys.process._
+      file(("which" :: "php" :: Nil).!!.trim)
+    },
     libraryDependencies ++= Seq(
       compilerPlugin(
         "org.scala-php" % "scala-php-plugin" % scalaPhpVersion.value cross CrossVersion.full
       )
     ),
+    run := {
+      val compiled = (Compile / compile).value
+      import sys.process._
+
+      val logger = (Compile / streams).value.log
+
+      (phpBinary.value.toString :: "demo.php" :: Nil)
+        .lineStream(logger)
+        .foreach(println)
+    },
   )
 
 }
---
+++ sbt-plugin/src/sbt-test/running/hello-world-run/src/main/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+object Demo {
+
+  def main(
+    args: Array[String]
+  ): Unit = {
+    val s = "world"
+    println(s"hello $s!")
+  }
+
+}
