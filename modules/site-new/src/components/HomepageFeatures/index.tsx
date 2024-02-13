bba15fc9e40d7261267e8bef6de3f235cf6d2683
Jakub Kozłowski <kubukoz@gmail.com>
2024-02-13 19:28:07 +0100

    Update icons and features

--- modules/site-new/src/components/HomepageFeatures/index.tsx
+++ modules/site-new/src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,53 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  title: JSX.Element;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    /* love */
+    title: <>Your favorite language... transpiled from Scala!</>,
+    Svg: require("@site/static/img/undraw_showing_support_re_5f2v.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Scala.php lets you use the features of Scala to build applications in
+        the language you love, PHP.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    /* collaboration,aliens,  */
+    title: <>Seamless interoperability with PHP</>,
+    Svg: require("@site/static/img/undraw_connection_re_lcud.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        You can mix Scala and PHP syntax in the same project, allowing you to
+        benefit from the best of both worlds.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    /* sharing, giving, copying, new life */
+    title: <>Reuse Scala Libraries</>,
+    Svg: require("@site/static/img/undraw_building_blocks_re_5ahy.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Just like Scala.js and Scala Native, Scala.php allows you to use the
+        entire ecosystem of Scala libraries - as long as they publish
+        Scala.php-compatible artifacts.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
